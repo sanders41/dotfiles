a834d30ec04b9ed140400a178c292526e7e11dd7
Paul Sanders <psanders1@gmail.com>
2022-09-11 09:30:28 -0400

    Move file name in lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -10,8 +10,8 @@ lualine.setup({
   },
   sections = {
     lualine_a = {"mode"},
-    lualine_b = {{"filename", path=2}, "branch", "diff", "diagnostics"},
-    lualine_c = {},
+    lualine_b = {"branch", "diff", "diagnostics"},
+    lualine_c = {{"filename", path=2}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {"progress"},
     lualine_z = {"location"},
