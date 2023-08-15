9d37eb44b0014be04a879d9abfe0016d9c80b4fd
Paul Sanders <psanders1@gmail.com>
2023-08-15 10:07:18 -0400

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,6 @@ local wezterm = require 'wezterm'
 -- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
@@ -17,6 +15,13 @@ config.font = wezterm.font 'JetBrains Mono'
 
 -- Disable ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+config.scrollback_lines = 5000
+
+-- Tab bar
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 -- and finally, return the configuration to wezterm
 return config
