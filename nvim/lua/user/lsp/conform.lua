6c50fe94a051c77c75a8310cac55d90526b78e3d
Paul Sanders <psanders1@gmail.com>
2023-10-26 14:17:28 -0400

    Improve Python formatter loading

--- nvim/lua/user/lsp/conform.lua
+++ nvim/lua/user/lsp/conform.lua
@@ -1,5 +1,6 @@
-local function python_formatter()
-  local status_ok, python_packages = pcall(io.popen, "pip freeze")
+local function get_python_formatter()
+  -- 2>&1 to also capture stderr
+  local status_ok, python_packages = pcall(io.popen, "pip freeze 2>&1")
   if not status_ok then
     if python_packages ~= nil then
       python_packages:close()
@@ -8,48 +9,72 @@ local function python_formatter()
   end
   if python_packages ~= nil then
     local black = false
+    local isort = false
     local ruff = false
     for package in python_packages:lines() do
-      if string.find(package, "black") then
+      if string.find(package, "ERROR:") then
+        python_packages:close()
+        return
+      elseif string.find(package, "black") then
         black = true
-      end
-
-      if string.find(package, "ruff") then
+      elseif string.find(package, "isort") then
+        isort = true
+      elseif string.find(package, "ruff") then
         ruff = true
       end
 
-      if black and ruff then
+      if black and isort and ruff then
         break
       end
     end
 
     python_packages:close()
 
-    if black and ruff then
-      return { "ruff_fix", "black" }
-    elseif black then
-      return { "black" }
-    elseif ruff then
-      return { "ruff_fix", "ruff_format" }
+    local packages = {}
+    if isort then
+      table.insert(packages, "isort")
     end
-  else
-    return
+
+    if ruff then
+      table.insert(packages, "ruff_fix")
+
+      if black then
+        table.insert(packages, "black")
+      else
+        table.insert(packages, "ruff_format")
+      end
+    elseif black then
+      table.insert(packages, "black")
+    end
+
+    if next(packages) == nil then
+      return
+    end
+
+    return packages
   end
 end
 
+local formatters = {
+  css = { { "prettierd", "prettier" } },
+  go = { "gofmt" },
+  html = { { "prettierd", "prettier" } },
+  javascript = { { "prettierd", "prettier" } },
+  lua = { "stylua" },
+  markdown = { { "prettierd", "prettier" } },
+  rust = { "rustfmt" },
+  typescript = { { "prettierd", "prettier" } },
+  yaml = { { "prettierd", "prettier" } },
+}
+
+local python_formatter = get_python_formatter()
+
+if python_formatter ~= nil then
+  formatters.python = python_formatter
+end
+
 require("conform").setup({
-  formatters_by_ft = {
-    css = { { "prettierd", "prettier" } },
-    go = { "gofmt" },
-    html = { { "prettierd", "prettier" } },
-    javascript = { { "prettierd", "prettier" } },
-    lua = { "stylua" },
-    markdown = { { "prettierd", "prettier" } },
-    python = python_formatter(),
-    rust = { "rustfmt" },
-    typescript = { { "prettierd", "prettier" } },
-    yaml = { { "prettierd", "prettier" } },
-  },
+  formatters_by_ft = formatters,
   format_on_save = {
     timeout_ms = 500,
     lsp_fallback = true,
