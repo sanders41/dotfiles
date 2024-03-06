1f48064c3b09569a61b431590998eebfc6824995
Paul Sanders <psanders1@gmail.com>
2024-03-06 08:48:09 -0500

    Add descriptions to keymaps

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -58,27 +58,28 @@ local function lsp_highlight_document(client)
 end
 
 local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  -- local opts = { noremap = true, silent = true }
+  local function buf_set_keymap(keys, func, desc)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", keys, func, { desc, desc })
+  end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration")
+  buf_set_keymap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition")
+  buf_set_keymap("K", "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover documentation")
+  buf_set_keymap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", "Go to implementation")
+  buf_set_keymap("<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add workspace folder")
+  buf_set_keymap("<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove workspace folder")
+  buf_set_keymap(
+    "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "List workspace folders"
+  )
+  buf_set_keymap("<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", "Type definitions")
+  buf_set_keymap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename vairable")
+  buf_set_keymap("<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code action")
+  buf_set_keymap("gr", "<cmd>lua vim.lsp.buf.references()<CR>", "Go to reference")
+  buf_set_keymap("<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", "Show diagnostics")
+  buf_set_keymap("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", "Go to previous diagnostic")
+  buf_set_keymap("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Go to next diagnostic")
   vim.cmd [[ command! Format execute "lua vim.lsp.buf.formatting()" ]]
 end
 
