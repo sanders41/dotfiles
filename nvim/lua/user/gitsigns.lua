910c3c43c48b50fff50ffe8fba73be4268bc82d8
Paul Sanders <paul@paulsanders.dev>
2024-06-20 19:28:12 -0400

    Fix gitsigns depreciation warnings

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -6,11 +6,11 @@ end
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { text = "│" },
+    change = { text = "│" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -28,9 +28,6 @@ gitsigns.setup {
     delay = 2000,
     ignore_whitespace = false,
   },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
-  },
   current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary> | commit: <sha>",
   current_line_blame_formatter_nc = "    Not Committed Yet",
   sign_priority = 6,
@@ -44,8 +41,5 @@ gitsigns.setup {
     relative = "cursor",
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
   },
 }
