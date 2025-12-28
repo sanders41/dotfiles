f8730484d2bd2229708f89ca5edf2f20ab8447fa
Paul Sanders <paul@paulsanders.dev>
2025-12-28 08:06:25 -0500

    Remove pyenv and update uv to manage Python

--- run_updates.sh
+++ run_updates.sh
@@ -12,11 +12,21 @@ printf "\n${BLUE}Updating NPM packages${RESET}\n"
 npm -g update
 printf "\n${BLUE}Updating uv${RESET}\n"
 uv self update
-printf "\n${BLUE}Updating uv tool packages${RESET}\n"
-uv tool upgrade --all
+
+printf "\n${BLUE}Check for Python updates${RESET}\n"
+current_version = $(uv run python -V 2>&1 | rg -o '\d+\.\d+\.\d+')
+new_version = $(uv python install 3.14 --reinstall 2>&1 | rg -o '\d+\.\d+\.\d+' | head -n1)
+if [[ "$current_version" == "$new_version" ]] ; then
+  printf "\n${BLUE}No new Python version, updating uv tool packages${RESET}\n"
+  uv tool upgrade --all
+else
+  printf "\n${BLUE}New Python version, reinstalling uv tool packages${RESET}\n"
+  uv tool upgrade --all --reinstall
+fi
+
 printf "\n${BLUE}Updating rust${RESET}\n"
 rustup update
 printf "\n${BLUE}Updating cargo packages${RESET}\n"
 cargo install-update -a
-printf "\n${BLUE}Updating pyenv${RESET}\n"
-git -C $HOME/.pyenv pull
+# printf "\n${BLUE}Updating pyenv${RESET}\n"
+# git -C $HOME/.pyenv pull
